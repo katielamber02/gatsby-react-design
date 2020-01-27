782c50bb9b7ec7efa3ded05f7672419ec314b003
Katie Lamber <katielamber02@gmail.com>
2020-01-27 18:12:57 +0200

    header onscroll event

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,41 @@
 import { Link } from "gatsby"
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 import logo from "../images/logo-designcode.svg"
 import "./Header.css"
 
-const Header = () => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={logo} width="30" alt="logotext" />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
+const Header = () => {
+  const [hasScrolled, setHeader] = useState(false)
+  const handleScroll = event => {
+    console.log(event)
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      setHeader(true)
+    } else {
+      setHeader(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  }, [hasScrolled])
+
+  return (
+    <div className={hasScrolled ? "Header HeaderScrolled" : "Header"}>
+      <div className="HeaderGroup">
+        <Link to="/">
+          {/* <img src={logo} width="30" alt="logotext" /> */}
+          <img src={require("../images/logo-designcode.svg")} width="50" />
+        </Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy">
+          <button>Buy</button>
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
