b2a03849a621b197dbfb68e81896b065a864cf89
Katie Lamber <katielamber02@gmail.com>
2020-01-27 17:21:27 +0200

    cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Card from "./../components/Card"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -48,6 +50,31 @@ const IndexPage = () => (
             />
           </path>
         </svg>
+      </div>
+      <div className="Cards">
+        <h2>11 courses, more coming</h2>
+        <div className="CardGroup">
+          <Card
+            title="Design System"
+            text="10 sections"
+            image={require("../images/wallpaper.jpg")}
+          />
+          <Card
+            title="React for Designers"
+            text="12 sections"
+            image={require("../images/wallpaper2.jpg")}
+          />
+          <Card
+            title="Sound Design"
+            text="5 sections"
+            image={require("../images/wallpaper3.jpg")}
+          />
+          <Card
+            title="ARKit 2"
+            text="10 sections"
+            image={require("../images/wallpaper4.jpg")}
+          />
+        </div>
       </div>
     </div>
   </Layout>
